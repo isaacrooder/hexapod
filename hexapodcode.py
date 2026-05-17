c61afb736ca4175b7716363dd07b36daa7cfe425
Seth Github <srooder@gmail.com>
2026-05-17 10:31:15 -0400

    Add hexapod control logic in hexapodcode.py
    Implement hexapod control with servo and leg classes.

---
+++ hexapodcode.py
@@ -0,0 +1,28 @@
+import libary
+import pigpio
+from time import sleep
+
+pi = pigpio.pi()
+base = libary.Servo(pi, 18)
+hip = libary.Servo(pi, 4)
+basetwo = libary.Servo(pi, 27)
+hiptwo = libary.Servo(pi, 2)
+
+legtwo = libary.Leg(hiptwo, basetwo)
+leg = libary.Leg(hip, base)
+while True:
+	leg.liftleft()
+	legtwo.liftright()
+	sleep(1)
+	leg.forwardleft()
+	legtwo.forwardright()
+	sleep(1)
+	leg.lowerleft()
+	legtwo.lowerright
+	sleep(1)
+	leg.center()
+	legtwo.center()
+	sleep(1)
+
+
+
